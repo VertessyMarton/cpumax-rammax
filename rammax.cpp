d6d5c6a56fd0fd76d2bcaafcc6d76778abdb0d31
VertessyMarton <72082226+VertessyMarton@users.noreply.github.com>
2025-02-22 20:15:21 +0100

    Add files via upload

---
+++ rammax.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <thread>
+#include <chrono>
+
+int main() {
+    std::vector<void*> a;
+    while (true) {
+        void* b = std::malloc(10 * 1024 * 1024);
+        if (b) {
+            std::memset(b, 0, 10 * 1024 * 1024);
+            a.push_back(b);
+        }
+        
+    }
+    return 0;
+}
